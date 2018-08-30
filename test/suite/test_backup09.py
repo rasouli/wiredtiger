8b423cbcf1b317bf617ff54f787e1d7f3b544bd6
Daniel Gottlieb <daniel.gottlieb@mongodb.com>
2018-08-30 14:38:27 -0400

    WT-4177: Force a log switch when opening a backup cursor.

--- test/suite/test_backup09.py
+++ test/suite/test_backup09.py
@@ -34,22 +34,20 @@ import os, shutil
 import wiredtiger, wttest
 from wtscenario import make_scenarios
 
-def timestamp_str(t):
-    return '%x' % t
-
 class test_backup09(wttest.WiredTigerTestCase):
     conn_config = 'config_base=false,create,log=(enabled)'
     uri = 'table:coll1'
     dir = 'backup.dir'
 
     types = [
-        ('checkpoint', {"checkpoint": True}),
-        ('no_checkpoint', {"checkpoint": False}),
+        ('checkpoint', {'checkpoint': True}),
+        ('no_checkpoint', {'checkpoint': False}),
     ]
     scenarios = make_scenarios(types)
 
     def data_and_start_backup(self):
         self.session.create(self.uri, 'key_format=i,value_format=i')
+
         cursor = self.session.open_cursor(self.uri)
         doc_id = 0
 
@@ -64,42 +62,57 @@ class test_backup09(wttest.WiredTigerTestCase):
             doc_id += 1
             cursor[doc_id] = doc_id
 
-        last_doc = doc_id
+        last_doc_in_backup = doc_id
         self.assertEqual(1, len(filter(lambda x: x.startswith('WiredTigerLog.'),
-                                       shutil.os.listdir('.'))))
+                                        os.listdir('.'))))
         backup_cursor = self.session.open_cursor('backup:')
+        # Opening a backup cursor will immediately force a new log file.
         self.assertEqual(2, len(filter(lambda x: x.startswith('WiredTigerLog.'),
-                                       shutil.os.listdir('.'))))
+                                        os.listdir('.'))))
 
         for i in range(10):
             doc_id += 1
             cursor[doc_id] = doc_id
 
         cursor.close()
+        # Flush to log to ensure all files contain all expected writes.
+        # Otherwise a logical failure (containing more data than expected) may
+        # go unnoticed.
         self.session.log_flush('sync=on')
-        return backup_cursor, doc_id
+        return backup_cursor, last_doc_in_backup
 
     def copy_and_restore(self, backup_cursor, last_expected_doc):
+        log_files_to_copy = 0
         os.mkdir(self.dir)
         while True:
             ret = backup_cursor.next()
             if ret != 0:
                 break
             shutil.copy(backup_cursor.get_key(), self.dir)
-            print("Copying: {}".format(backup_cursor.get_key()))
+            if backup_cursor.get_key().startswith('WiredTigerLog.'):
+                log_files_to_copy += 1
+
         self.assertEqual(ret, wiredtiger.WT_NOTFOUND)
+        self.assertEqual(log_files_to_copy, 1)
 
         backup_conn = self.wiredtiger_open(self.dir, self.conn_config)
-        backup_session = backup_conn.open_session()
-        backup_cursor = backup_session.open_cursor(self.uri)
-        for key, val in backup_cursor:
-            print("Key: {} Val: {}".format(key, val))
+        session = backup_conn.open_session()
+        cursor = session.open_cursor(self.uri)
 
-    def test_timestamp_backup(self):
-        # Add some data, open a backup cursor, and add some more data. Return the value of the last document that should appear on a restore.
+        doc_cnt = 0
+        for key, val in cursor:
+            doc_cnt += 1
+            self.assertLessEqual(key, last_expected_doc)
+
+        self.assertEqual(doc_cnt, last_expected_doc)
+
+    def test_backup_rotates_log(self):
+        # Add some data, open a backup cursor, and add some more data. Return
+        # the value of the last document that should appear on a restore.
         backup_cursor, last_doc = self.data_and_start_backup()
 
-        # Copy the files returned via the backup cursor and bring up WiredTiger on the destination. Verify no document later than last_doc exists.
+        # Copy the files returned via the backup cursor and bring up WiredTiger
+        # on the destination. Verify no document later than last_doc exists.
         self.copy_and_restore(backup_cursor, last_doc)
 
 if __name__ == '__main__':
